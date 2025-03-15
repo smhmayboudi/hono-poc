6ccba1409bc0bd9b764ed3e5a993ce7ca5c61f44
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-03-15 18:37:40 +0330

    refactor: update app event emitter

--- src/app.event-emitter.ts
+++ src/app.event-emitter.ts
@@ -1,6 +1,6 @@
 import { eq } from "drizzle-orm";
 
-import { userPOCView as databaseUserPOCView } from "./infrastructure/adapter/database/schema/view/user-poc-view.ts";
+import { userPOCView } from "./infrastructure/adapter/database/schema/view/user-poc-view.ts";
 import type { PortCacher } from "./infrastructure/application/port/cacher/cacher.ts";
 import type { PortDatabase } from "./infrastructure/application/port/database/database.ts";
 import type { PortElasticsearch } from "./infrastructure/application/port/elasticsearch/elasticsearch.ts";
@@ -24,8 +24,8 @@ export const appEventEmitter = (
     const results = await database
       .db()
       .select()
-      .from(databaseUserPOCView)
-      .where(eq(databaseUserPOCView.user_poc_id, data.response.id))
+      .from(userPOCView)
+      .where(eq(userPOCView.user_poc_id, data.response.id))
       .execute();
     for (const result of results) {
       await elasticsearch.client().create({
@@ -66,8 +66,8 @@ export const appEventEmitter = (
     const results = await database
       .db()
       .select()
-      .from(databaseUserPOCView)
-      .where(eq(databaseUserPOCView.user_poc_id, data.request.id))
+      .from(userPOCView)
+      .where(eq(userPOCView.user_poc_id, data.request.id))
       .execute();
     for (const result of results) {
       await elasticsearch.client().update({
