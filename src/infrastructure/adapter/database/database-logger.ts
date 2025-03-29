2675348a2e624c7a480bde5a866e7d4fe5703629
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-03-29 13:50:26 +0330

    refactor: add database logger

---
+++ src/infrastructure/adapter/database/database-logger.ts
@@ -0,0 +1,22 @@
+import { ATTR_CODE_FUNCTION_NAME } from "@opentelemetry/semantic-conventions/incubating";
+import type { Logger } from "drizzle-orm";
+
+import type { PortConfig } from "../../application/port/config/config.ts";
+import type { PortLogger } from "../../application/port/logger/logger.ts";
+
+export class DatabaseLogger implements Logger {
+  constructor(
+    private readonly config: PortConfig,
+    private readonly logger: PortLogger,
+  ) {}
+
+  logQuery(query: string, params: unknown[]): void {
+    this.logger.assign({
+      [ATTR_CODE_FUNCTION_NAME]: "logQuery-database.infrastructure",
+      config: this.config,
+      params,
+      query,
+    });
+    this.logger.debug({});
+  }
+}
--- src/infrastructure/adapter/database/database.ts
+++ src/infrastructure/adapter/database/database.ts
@@ -1,5 +1,3 @@
-import { ATTR_CODE_FUNCTION_NAME } from "@opentelemetry/semantic-conventions/incubating";
-import type { Logger } from "drizzle-orm/logger";
 import { drizzle, type MySql2Database } from "drizzle-orm/mysql2";
 import { createPool, type Pool } from "mysql2";
 
@@ -8,23 +6,7 @@ import type { PortDatabase } from "../../application/port/database/database.ts";
 import * as schema from "../../application/port/database/schema/schema.ts";
 import type { PortLogger } from "../../application/port/logger/logger.ts";
 import { tracer } from "../opentelemetry/opentelemetry.ts";
-
-export class Logger2 implements Logger {
-  constructor(
-    private readonly config: PortConfig,
-    private readonly logger: PortLogger,
-  ) {}
-
-  logQuery(query: string, params: unknown[]): void {
-    this.logger.assign({
-      [ATTR_CODE_FUNCTION_NAME]: "logQuery-database.infrastructure",
-      config: this.config,
-      params,
-      query,
-    });
-    this.logger.debug({});
-  }
-}
+import { DatabaseLogger } from "./database-logger.ts";
 
 export class Database2 implements PortDatabase {
   constructor(
@@ -37,7 +19,7 @@ export class Database2 implements PortDatabase {
   } {
     return drizzle(this.pool(), {
       casing: "snake_case",
-      logger: new Logger2(this.config, this.logger),
+      logger: new DatabaseLogger(this.config, this.logger),
       mode: "default",
       schema,
     });
