58f9a983a9937efb307e964629cfbcb99f0dc493
Hossein Mayboudi <smhmayboudi@gmail.com>
2025-05-06 09:47:43 +0330

    refactor: remove extra utils

--- src/util/object-properties-filter-undefined.ts
+++
@@ -1,8 +0,0 @@
-export const objectPropertiesFilterUndefined = <
-  T extends Record<string, unknown>,
->(
-  obj: T,
-): { [K in keyof T]: Exclude<T[K], undefined> } =>
-  Object.fromEntries(Object.entries(obj).filter(([, value]) => value)) as {
-    [K in keyof T]: Exclude<T[K], undefined>;
-  };
